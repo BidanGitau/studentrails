981200a60cfeedb27e4f31e84a841d756367c91f
BidanGitau <bydangitau@gmail.com>
2022-05-13 17:01:44 +0300

    user listing page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,44 +1,39 @@
 class UsersController < ApplicationController
-    def index
-        users=User.all
-    end
-
-def new
-    @user=User.new
-end
     def show
-        @user=User.find(params[:id])
-        @articles=@user.articles
+      @user = User.find(params[:id])
+      @articles = @user.articles
+    end
+    def index
+      @users = User.all
+    end
+    def new
+      @user = User.new
+    end
+    def edit
+      @user = User.find(params[:id])
+    end
+    def update
+      @user = User.find(params[:id])
+      if @user.update(user_params)
+        flash[:notice] = "Your account information was successfully updated"
+       
+        redirect_to @user
+      else
+        render 'edit'
+      end
     end
     def create
-        @user=User.new(user_params)
-        if @user.save
-        flash[:notice]="Welcome to school of academics #{@user.username}"
+      @user = User.new(user_params)
+      if @user.save
+        flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signed up"
         redirect_to articles_path
-    
-    else
+      else
         render 'new'
+      end
     end
-    end
-
-    def edit
-        @user=User.find(params[:id])
-    end
-
-    def update
-        @user=User.find(params[:id])
-        if @user.update(user_params)
-            flash[:notice]="success info update #{@user.username}"
-            redirect_to @user
-        else
-            render 'edit'
-            end
-    end
-
-
-
     private
     def user_params
-        params.require(:user).permit(:username,:email,:password)
+      params.require(:user).permit(:username, :email, :password)
     end
-end
+    
+  end
