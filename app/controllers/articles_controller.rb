0c6af0333f7c6ffff440075cd20813be026d357c
BidanGitau <bydangitau@gmail.com>
2022-05-17 10:45:21 +0300

    user and admin auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article,only:[:show,:edit,:destroy,:update]
+  before_action :require_user , except:[:show,:index]
+  before_action :require_same_user , only:[:edit,:iupdate,:destroy]
   
 
   def show
@@ -20,7 +22,7 @@ class ArticlesController < ApplicationController
    
   def create
       @article = Article.new(article_params)
-      @article.user=User.first
+      @article.user=current_user
       if @article.save
         flash[:notice] = "Article was created successfully."
         redirect_to @article 
@@ -55,6 +57,11 @@ class ArticlesController < ApplicationController
       params.require(:article).permit(:title, :description)
 
     end
-    
+    def require_same_user
+      if current_user != @article.user && !current_user.admin
+        flash[:alert] = "Can only edit yuor posts"
+        redirect_to @article
+      end
+    end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_12_035420) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_17_070118) do
   create_table "articles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_12_035420) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
